59d3702838be44152df54557113226bfe2486c6e
CasManny <chukwusabastine2018@gmail.com>
2022-02-17 02:14:26 +0100

    set up User model using mongoose for creating users for the app

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter a name"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        minLength: [3, "password should be more than 3 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, "please Enter an email"],
+        unique: true,
+    }
+}, { timeStamps: true})
+
+userSchema.pre('save', async function() {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+
+
+module.exports = mongoose.model("User", userSchema)
